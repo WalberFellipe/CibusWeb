7124e32f5f83128dfeb8a107ecf122338df4f5a6
Walber Fellipe <walber.fellipe@cibus.app.br>
2022-03-14 00:12:21 -0300

    Button style

---
+++ src/components/DemoButton/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+export function DemoButton() {
+  return (
+    <button type='button' className={styles.demoButton}>
+      QUERO UMA DEMONSTRAÇÃO
+    </button>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import { DemoButton } from '../components/DemoButton';
+
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
@@ -6,7 +9,27 @@ export default function Home() {
       <Head>
         <title>Início | CIBU$ Fidelidade</title>
       </Head>
-      <h1>Olá Mundo!</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <h1>
+            Aumente suas venda com
+            <h1>CASHBACK</h1>
+          </h1>
+
+          <p>
+            Da atração ao aumento do ticket médio: uma solução completa para
+            engajar clientes, motivar atendentes e acelerar vendas.
+            <br />
+            <br />
+            <span>Simples e 100% personalizável.</span>
+          </p>
+
+          <DemoButton />
+        </section>
+
+        <img src='/images/cellphones.png' alt='Cellphones' />
+      </main>
     </>
   );
 }
