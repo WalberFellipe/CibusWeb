de794caebb8e07b1df51ae08dd701331c1e5176b
Walber Fellipe <walber.fellipe@cibus.app.br>
2022-03-14 02:03:58 -0300

    First section done, now I'm going to do second and third and style them

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,8 @@ export default function Home() {
         <title>Início | CIBU$ Fidelidade</title>
       </Head>
 
+      {/* PRIMEIRA SEÇÃO */}
+
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <h1>
@@ -24,11 +26,60 @@ export default function Home() {
             <br />
             <span>Simples e 100% personalizável.</span>
           </p>
+          <br />
 
           <DemoButton />
         </section>
 
         <img src='/images/cellphones.png' alt='Cellphones' />
+      </main>
+
+      {/* SEGUNDA SEÇÃO */}
+
+      <main className={styles.secondSection}>
+        <section className={styles.secondSectionText}>
+          <h1>
+            O seu <span>acelerador de vendas</span> no posto
+          </h1>
+
+          <p>
+            Com o CIBUS você acelera as vendas do posto e multiplica as chances
+            do seu cliente voltar ao oferecer cashback instantâneo. Ou seja,
+            parte do valor gasto no abastecimento é revertido na compra de novos
+            produtos da loja de conveniência, serviços, futuros abastecimentos
+            no posto ou em produtos de lojas parceiras.
+          </p>
+        </section>
+      </main>
+
+      {/* TERCEIRA SEÇÃO */}
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <h1>
+            Seu App com a cara da
+            <h1>
+              <span>sua empresa</span>
+            </h1>
+          </h1>
+
+          <p>
+            Tenha seu próprio app de cashback ao personalizar o Cibus com a logo
+            e as cores do seu posto.
+          </p>
+          <br />
+          <p>
+            O app é um “white-label”, ou seja, um produto de marca branca que é
+            desenvolvido por nós, mas que você pode inserir a sua marca, como se
+            fosse uma tecnologia sua, do seu posto.
+          </p>
+
+          <br />
+        </section>
+
+        <h1>
+          Personalize <span>VOCÊ</span> mesmo!
+        </h1>
       </main>
     </>
   );
